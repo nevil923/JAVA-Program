8029ea781cfa7519e78365b1459eee5ca5c79742
nevil923 <nevil.sakariya140231@marwadiuniversity.ac.in>
2026-08-02 12:02:30 +0530

    Create Program_6Ex_2.java

---
+++ Program_6Ex_2.java
@@ -0,0 +1,27 @@
+//6.2
+
+import java.util.Scanner;
+
+	public class Program_6Ex_2
+	{
+		public static void main(String args[])
+		{
+			Scanner scn = new Scanner(System.in);
+            System.out.println("Enter the integer:");
+			int Userinteger=scn.nextInt();
+			
+			scn.close();
+			
+			System.out.println("Entered integer: " + Userinteger);
+			int i=1;
+			 while (  i <= 5)
+			 {
+              System.out.println(Userinteger);
+			  i++;
+             }
+			
+  		   
+  	 
+   
+  }
+}
