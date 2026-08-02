73a9ce2f67babc4030707d2ee487006ab60f52ad
nevil923 <nevil.sakariya140231@marwadiuniversity.ac.in>
2026-08-02 12:53:03 +0530

    Add files via upload

---
+++ Program_6Ex_8.java
@@ -0,0 +1,13 @@
+//6.8
+
+public class Program_6Ex_8
+
+{
+    public static void main(String args[]) {
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("Userstr");
+            i++;
+        }
+    }
+}
