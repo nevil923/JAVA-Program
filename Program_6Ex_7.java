0bf3a9f704d9edf995b0d3132699b5e6ce04e040
nevil923 <nevil.sakariya140231@marwadiuniversity.ac.in>
2026-08-02 12:52:44 +0530

    Add files via upload

---
+++ Program_6Ex_7.java
@@ -0,0 +1,12 @@
+//6.7
+
+public class Program_6Ex_7
+
+{
+    public static void main(String args[]) {
+        int i;
+        for (i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
