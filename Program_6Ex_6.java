8c09ac4089d25a0a0eddc626ec96f7ea416dec73
nevil923 <nevil.sakariya140231@marwadiuniversity.ac.in>
2026-08-02 12:52:24 +0530

    Add files via upload

---
+++ Program_6Ex_6.java
@@ -0,0 +1,23 @@
+//6.6
+
+import java.util.Scanner;
+
+public class Program_6Ex_6 {
+    public static void main(String args[]) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter the string:");
+        String Userstr = scn.nextLine();
+        System.out.println("Enter the limit:");
+
+        int num = scn.nextInt();
+        scn.close();
+
+        int i = 1;
+        do {
+            System.out.println(Userstr);
+            i++;
+        } while (i <= num);
+
+    }
+}
