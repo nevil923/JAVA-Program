b25728a52c958f9318e937ec4b70f55692fdc854
nevil923 <nevil.sakariya140231@marwadiuniversity.ac.in>
2026-08-02 12:55:16 +0530

    Add files via upload

---
+++ Program_7Ex_7.java
@@ -0,0 +1,39 @@
+//7.7
+
+import java.util.Scanner;
+
+public class Program_7Ex_7
+
+{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter the day of number:");
+        int week = scn.nextInt();
+        switch (week) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid day");
+        }
+    }
+}
