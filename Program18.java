2da8c9b9f8613d861e6d3e0dcb9ed71dadee23f5
NevilSakariya93 <nevil.sakariya140231@marwadiuniversity.ac.in>
2026-08-19 15:41:08 +0530

    Add files via upload

---
+++ Program18.java
@@ -0,0 +1,11 @@
+public class Program18
+{
+	public static void main(String[] args)
+	{
+		final int JALPIN_ROLL_NO = 105;
+		final double MAX_MARKS = -5.00;
+		
+		System.out.println("JALPIN's ROLL NO: " + JALPIN_ROLL_NO);
+		System.out.println("MARK's: " + MAX_MARKS);
+	}
+}
