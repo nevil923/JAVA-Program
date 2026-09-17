d3d375613cb81169f81c9fd95e5f741f50a16e59
NevilSakariya93 <nevil.sakariya140231@marwadiuniversity.ac.in>
2026-09-17 09:30:13 +0530

    Add files via upload

---
+++ Unit-3/TestTryCatch.java
@@ -0,0 +1,24 @@
+public class TestTryCatch
+{
+	public static void main(String[] args)
+	{
+		String studentName = "Vivaan";
+		System.out.println(studentName + " started math calculation...");
+		
+		try
+		{
+			int totalMarks=100;
+			int subject = 0;
+			int result = totalMarks / subject;
+			
+			System.out.println("Result: " + result);
+		}
+		catch (ArithmeticException e)
+		{
+			System.out.println(Exception Caught: Division by zero is not allowed for " 
+			+ studentName + "!");
+			System.out.println("System Error Detail: " + e.getMessage());
+		}
+		System.out.println(studentName + "'s program continues execution smoothly!");
+	}
+}
